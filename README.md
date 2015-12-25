e1e051b798fe87a5d370092e1b50803604ab5762
Steven Dale <lifeinchords@gmail.com>
2015-12-24 23:00:53 -0500

    set

---
+++ README.md
@@ -0,0 +1,52 @@
+# start with Gulp + PostCSS
+https://github.com/azat-io/generator-alchemist#readme
+
+# set up LostCss
+npm install --save-dev gulp gulp-postcss gulp-sourcemaps autoprefixer lost
+
+<!-- npm install postcss-use --save -->
+
+
+
+GRID
+------
+
+http://leveluptuts.com/tutorials/postcss-tutorials/7-lost-next-level-grid-system
+
+
+
+
+
+
+
+BEM
+-----
+
+http://csswizardry.com/2013/01/mindbemding-getting-your-head-round-bem-syntax/
+http://blog.trello.com/refining-the-way-we-structure-our-css-at-trello/
+http://glenmaddern.com/articles/introducing-am-css
+https://github.com/davidtheclark/scalable-css-reading-list
+https://medium.com/@ddprrt/postcss-misconceptions-faf5dc5038df#.fmtfvhykd
+https://benfrain.com/breaking-up-with-sass-postcss/
+http://leveluptuts.com/tutorials/postcss-tutorials/7-lost-next-level-grid-system
+http://www.sitepoint.com/bem-smacss-advice-from-developers/
+http://nicolasgallagher.com/about-html-semantics-front-end-architecture/ 
+
+
+POSTCSS
+-------
+
+http://davidtheclark.com/its-time-for-everyone-to-learn-about-postcss/
+https://github.com/markgoodyear/postcss-vertical-rhythm
+https://github.com/postcss/postcss/blob/master/docs/plugins.md
+
+
+
+OFF CANVAS
+------
+http://codepen.io/dennisgaebel/pen/avOjMr
+http://codepen.io/rickzanutta/pen/QwwNEg
+http://codepen.io/anon/pen/rxMVMO
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+var batch = require('gulp-batch');
+var browserSync = require('browser-sync').create();
+var clean = require('gulp-clean');
+var clearFix = require('postcss-clearfix');
+var colorShort = require('postcss-color-short');
+var cssMqpacker = require('css-mqpacker');
+var cssNano = require('cssnano');
+var cssNext = require('postcss-cssnext');
+var discardComments = require('postcss-discard-comments');
+var focus = require('postcss-focus');
+var gulp = require('gulp');
+var htmlHint = require('gulp-htmlhint');
+var imageOp = require('gulp-image-optimization');
+var postcss = require('gulp-postcss');
+var precss = require('precss');
+var px2Rem = require('postcss-pxtorem');
+var responsiveImages = require('postcss-responsive-images');
+var short = require ('postcss-short');
+var size = require('postcss-size');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var lost = require('lost');
+
+
+gulp.task('default', ['server'], function() {
+  gulp.watch('src/html/**', function(event) {
+    gulp.run('html');
+  });
+  gulp.watch('src/css/**', function(event) {
+    gulp.run('postcss');
+  });
+  gulp.watch('src/js/**', function(event) {
+    gulp.run('js');
+  });
+  gulp.watch('src/images/**/*', batch(function (events, done) {
+      gulp.start('images', done);
+  }));
+});
+
+// HTML
+
+gulp.task('html', function() {
+  gulp.src('src/html/**/*.html')
+    .pipe(htmlHint())
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.stream());
+});
+
+// PostCSS
+
+gulp.task('postcss', function () {
+  var processors = [
+    // colorShort,
+    // focus,
+    // precss,
+    // short,
+    // size,
+    // responsiveImages,
+    // clearFix,
+    // px2Rem,
+    cssNext,
+    cssMqpacker,
+    // discardComments,
+    // cssNano,
+    lost,
+    autoprefixer
+  ];
+  return gulp.src('src/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.stream());
+});
+
+// JavaScript
+
+gulp.task('js', function () {
+  return gulp.src('src/js/*')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream());
+});
+
+// Image files
+
+gulp.task('images', function(cb) {
+  gulp.src(['src/images/**/*'])
+  .pipe(imageOp({
+    optimizationLevel: 5,
+    progressive: true,
+    interlaced: true
+  }))
+  .pipe(clean())
+  .pipe(gulp.dest('dist/images')).on('end', cb).on('error', cb)
+  .pipe(browserSync.stream());
+});
+
+// Server
+
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+      baseDir: "dist"
+    },
+    open: false
+  });
+});
